85a16aa15cff9b337f0909254600d72e2b9246da
Maryna Bonko <bonko23@gmail.com>
2023-05-07 23:29:20 +0300

    10.1

---
+++ src/partials/api-service.js
@@ -0,0 +1,10 @@
+// const BASE_URL = 'https://restcountries.com/v3.1';
+// function fetchCountryName(countryName) {
+//   return fetch(`${BASE_URL}/name/${countryName}?fullText=true`).then(
+//     response => {
+//       response.json();
+//     }
+//   );
+// }
+
+// export default { fetchCountryName };
