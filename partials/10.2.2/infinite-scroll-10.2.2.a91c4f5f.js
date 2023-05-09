db456197023ff8bc24dc0fca89bb337c20d47f7b
Verlong <103792395+Verlong@users.noreply.github.com>
2023-05-09 20:33:04 +0000

    Deploying to gh-pages from @ Verlong/goit-js-modules-practice@254586dc32689b2cb84e2b06aacd3673d7cb3c97 🚀

---
+++ partials/10.2.2/infinite-scroll-10.2.2.a91c4f5f.js
@@ -0,0 +1,2 @@
+var options={root:document.querySelector(".scroll-list"),rootMargin:"5px",threshold:.5},callback=function(e,r){e.forEach((function(e){}))},observer=new IntersectionObserver(callback),images=document.querySelectorAll("img");images.forEach((function(e){return observer.observe(e)}));
+//# sourceMappingURL=infinite-scroll-10.2.2.a91c4f5f.js.map
